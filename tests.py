86349de3dd96715fff9870904c61db8627256eac
Joshua <theunkn0wn1@users.noreply.github.com>
2017-12-03 18:22:35 -0800

    Add stub unit test file

---
+++ tests.py
@@ -0,0 +1,21 @@
+import unittest
+"""
+This is the Unit Test file for PipSqeak.
+Test Classes should be per-module
+"""
+
+
+class RatBoardTests(unittest.TestCase):
+    """
+    tests for the rat-board module
+    """
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':  # this prevents script code from being executed on import. (bad!)
+    unittest.main()
+
